74b27ea9558d171c4e6262c0aa295710b3c77e18
abhayHeliverse <siemonab@gmail.com>
2024-01-19 16:34:28 +0530

    deploying

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ import Profile from "./components/Profile";
 import LeaderBoard from "./components/LeaderBoard";
 
 // axios.defaults.baseURL = "http://localhost:8000/v1";
-axios.defaults.baseURL = "https://server-language-quiz.onrender.com";
+axios.defaults.baseURL = "https://server-language-quiz.onrender.com/v1";
 
 //If user is authorized then send user to the Protected Route else Navigate to auth page.
 const ProtectedRoute = ({ element, isAuthenticated }) => {
